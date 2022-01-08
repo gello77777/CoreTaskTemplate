0daea917fb6eecc983be21717c2c643168b13f19
gello77777 <gello77777@yandex.ru>
2022-01-08 10:46:38 +0700

    Start

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -18,6 +18,11 @@ public class User {
     @Column
     private Byte age;
 
+    @Override
+    public String toString() {
+        return "User с именем - " + name + " добавлен в базу данных";
+    }
+
     public User() {
 
     }
@@ -29,34 +34,42 @@ public class User {
     }
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public Byte getAge() {
+
         return age;
     }
 
     public void setAge(Byte age) {
+
         this.age = age;
     }
 }
